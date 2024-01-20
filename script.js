977cffb134b3b93f265d75b2e5a655744abc0662
Abhijith Subash <abhijithsubash07@gmail.com>
2024-01-20 16:35:17 +0530

    fix: tool open and close added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let menu = document.querySelector(".tool-menu")
+let toolContainer = document.querySelector(".tool-container")
 let menuFlag = true
 
 menu.addEventListener("click", (e) => {
@@ -10,10 +11,12 @@ function closeTools() {
     menuFlag = !menuFlag
     menu.children[0].classList.remove("fa-bars")
     menu.children[0].classList.add("fa-times")
+    toolContainer.style.display = "flex"
 }
 
 function openTools() {
     menuFlag = !menuFlag
     menu.children[0].classList.remove("fa-times")
     menu.children[0].classList.add("fa-bars")
+    toolContainer.style.display = "none"
 }
