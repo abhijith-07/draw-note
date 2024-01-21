0e97035caa6c7cb59763ad82dea60dc50ba7ff36
Abhijith Subash <abhijithsubash07@gmail.com>
2024-01-21 11:55:20 +0530

    fix: added open and close - pencil, eraser functions

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 let menu = document.querySelector(".tool-menu")
-let toolContainer = document.querySelector(".tool-container")
 let menuFlag = true
+let toolContainer = document.querySelector(".tool-container")
+let pencil = document.querySelector(".pencil")
+let pencilEdit = document.querySelector(".pencil-edit")
+let pencilFlag = false
+let eraser = document.querySelector(".eraser")
+let eraserEdit = document.querySelector(".eraser-edit")
+let eraserFlag = false
 
-menu.addEventListener("click", (e) => {
+// Menu open and close
+menu.addEventListener("click", () => {
     if (menuFlag) {closeTools()}
     else {openTools()}
+})
+
+// Pencil open and close
+pencil.addEventListener("click", () => {
+    if (pencilFlag) {closePencil()}
+    else {openPencil()}
+})
+
+// Eraser open and close
+eraser.addEventListener("click", ()=>{
+    if (eraserFlag) {closeEraser()}
+    else {openEraser()}
 })
 
 function closeTools() {
@@ -19,4 +38,24 @@ function openTools() {
     menu.children[0].classList.remove("fa-times")
     menu.children[0].classList.add("fa-bars")
     toolContainer.style.display = "none"
+}
+
+function openPencil() {
+    pencilFlag = !pencilFlag
+    pencilEdit.style.display = 'inline-block'
+}
+
+function closePencil() {
+    pencilFlag = !pencilFlag
+    pencilEdit.style.display = 'none'
+}
+
+function openEraser() {
+    eraserFlag = !eraserFlag
+    eraserEdit.style.display = 'inline-block'
+}
+
+function closeEraser() {
+    eraserFlag = !eraserFlag
+    eraserEdit.style.display = 'none'
 }
